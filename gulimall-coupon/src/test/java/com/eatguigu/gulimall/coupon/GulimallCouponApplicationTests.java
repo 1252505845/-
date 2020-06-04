770ad4e8297d009bb59ff19847e5de4598a01951
tom_glb <goodMorning_pro@atguigu.com>
2020-06-04 22:10:50 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/eatguigu/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatguigu.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
